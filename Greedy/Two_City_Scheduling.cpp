8447e09d239d925e41b8f48fff04992d89145862
snehaa1989 <61073116+snehaa1989@users.noreply.github.com>
2021-04-24 23:45:59 +0530

    Create Two_City_Scheduling.cpp

---
+++ Greedy/Two_City_Scheduling.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/two-city-scheduling/submissions/
+
+class Solution {
+public:
+    
+   int twoCitySchedCost(vector<vector<int>>& cs, int res = 0) {
+  nth_element(begin(cs), begin(cs) + cs.size() / 2, end(cs), [](vector<int> &a, vector<int> &b) {
+    return (a[0] - a[1] < b[0] - b[1]);
+  });
+  for (auto i = 0; i < cs.size() / 2; ++i) {
+    res += cs[i][0] + cs[i + cs.size() / 2][1];
+  }
+  return res;
+}
+};
